34ada0af95855189884e169e8cddd6c67339852c
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-29 21:43:16 +0900

    Update example application

--- cmd/colorpicker/main.go
+++ cmd/colorpicker/main.go
@@ -17,19 +17,29 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("color picker sample")
 
+	w.SetContent(fyne.NewContainerWithLayout(
+		layout.NewHBoxLayout(),
+		createPickerContainer(200, colorpicker.StyleDefault),
+		createPickerContainer(200, colorpicker.StyleCircle),
+	))
+
+	w.ShowAndRun()
+}
+
+func createPickerContainer(height int, style colorpicker.PickerStyle) *fyne.Container {
 	selectColorCode := widget.NewLabelWithStyle("", fyne.TextAlignLeading, fyne.TextStyle{Monospace: true})
 	selectColorRect := &canvas.Rectangle{FillColor: color.RGBA{0, 0, 0, 0}}
 	selectColorRect.SetMinSize(fyne.NewSize(30, 20))
 
 	// Create picker
-	picker := colorpicker.NewColorPicker(200, colorpicker.StyleDefault)
+	picker := colorpicker.NewColorPicker(height, style)
 	picker.Changed = func(c color.Color) {
 		selectColorCode.SetText(hexColorString(c))
 		selectColorRect.FillColor = c
 		selectColorRect.Refresh()
 	}
 
-	w.SetContent(fyne.NewContainerWithLayout(
+	return fyne.NewContainerWithLayout(
 		layout.NewVBoxLayout(),
 		picker.CanvasObject, // layout
 		fyne.NewContainerWithLayout(
@@ -39,9 +49,7 @@ func main() {
 			selectColorRect,
 			layout.NewSpacer(),
 		),
-	))
-
-	w.ShowAndRun()
+	)
 }
 
 func hexColorString(c color.Color) string {
